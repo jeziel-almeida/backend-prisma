98eaf35f0dc0bb0e8c90fcdc559ac3dcd4a8c867
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-11-15 22:05:44 +0000

    Add password hash

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,10 @@
 import { createUser } from "../repositories/user.repository";
+import bcrypt from "bcrypt";
 
 export const create = async (req, res) => {
     try {
+        const hashPassword = await bcrypt.hash(req.body.password, 10);
+        req.body.password = hashPassword;
         const user = await createUser(req.body);
         res.status(201).send(user);
     } catch (error) {
